cce22b79a607997b6f5baa3556eb99a9024ab703
joshbrown122222 <joshbrown2204@gmail.com>
2022-06-15 11:53:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+This is a simple startup project with compiles SCSS into CSS using gulp.
+
+The gulp file will look at the main.scss file within the /src/scss directory and compile it into CSS. The gulp file will compile the SCSS into both standard and minified (.min) CSS stylsheets using a map to help with style debugging. 
+
+Once compiled, the CSS files will be found within the /dist directory. **Please note** the /dist directory/folder is compiled and created when the gulp talk is run. No code changes should be made here! 
+
+Naturally, the index.html will look for the minified CSS file (main.min.css) but this can be changed within the <head></head> of the page by changing the href value of the stylesheet. 
+
+## Setup 
+1. If you do not have node.js. Go to [https://nodejs.org/en/](https://nodejs.org/en/) and download the latest and most stable version of node.js. This should also install npm (Node Package Manager).
+2. Install gulp and gulp cli globally by running `npm install -g gulp gulp-cli`
+3. Open your terminal within the project directory
+4. Run `npm install` 
+
+The following steps above only need to be done for the first time running of the project
+
+To compile and run the project, run `gulp` or `gulp watch` within a terminal opened at the root of this project. This should compile the CSS and run a localhost within your default browser. When a change is made within the index.html or any .scss files, the task will automatically compile the styles and reload the browser. 
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+
+//declare the scr folder
+let root = './src' + '/';
+let scssFolder = root + 'scss/';
+let allScssFiles = scssFolder + '**/*.scss';
+
+//declare the build folder
+let build = './dist/' + '/';
+let cssFolder = build + 'css';
+
+// Compile scss into css
+function css() {
+  return gulp
+    .src(scssFolder + '')
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      }).on('error', sass.logError)
+    )
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(cssFolder))
+    .pipe(browserSync.stream());
+}
+
+//minify css
+function minCSS() {
+  return gulp
+    .src(scssFolder + 'main.scss')
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(
+      sass({
+        outputStyle: 'compressed',
+      }).on('error', sass.logError)
+    )
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(cssFolder))
+    .pipe(browserSync.stream());
+}
+
+function watch() {
+
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+
+  gulp.watch(allScssFiles, gulp.series(css, minCSS));
+  gulp.watch('./*.html').on('change', browserSync.reload);
+
+}
+
+exports.css = css;
+exports.minCSS = minCSS;
+exports.compileAndMinify = gulp.series(css, minCSS);
+exports.watch = watch;
+exports.default = watch;
